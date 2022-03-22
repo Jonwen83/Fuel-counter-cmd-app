757e2e5c6629a7ec45c25554806c2a40cc46a96d
Jonwen83 <101714877+Jonwen83@users.noreply.github.com>
2022-03-22 14:07:36 +0200

    Added a method to count price per km

--- fuel.cpp
+++ fuel.cpp
@@ -4,14 +4,28 @@ int main() {
 
     double fuel;
     double kms;
+    char response;
+    double price;
 
     std::cout << "enter fuel in litres: \n";
     std::cin >> fuel;
     std::cout << "enter kms on trip meter: \n";
     std::cin >> kms;
 
-    std::cout << "your economy is\n " << fuel / kms * 100 << "litres per 100kms \n";
+    std::cout << "your economy is " << fuel / kms * 100 << "litres per 100kms \n";
+
+    std::cout << "would you like to calculate cost per km? y/n \n";
+    std::cin >> response;
+    if (!( response != 'Y' && response != 'y')) {
+        std::cout << "please enter fuel price in format 0.00: \n";
+        std::cin >> price;
+        std::cout << "Youve used " << fuel / kms * price << "\t euro per km \n";
+        std::cout << "press any key followed by enter to exit \n";
+        std::cin >> kms;
+    }
+    else {
 
     std::cout << "press any key followed by enter to exit \n";
     std::cin >> kms;
+    }
 }
